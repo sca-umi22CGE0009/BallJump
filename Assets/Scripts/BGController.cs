c458252d198486445606de9dc2783ffb6e42450c
sdt22cge0009 <sdt22cge0009@edu.sca.ac.jp>
2023-04-22 12:25:49 +0900

    横スクロールのスクリプト追加

---
+++ Assets/Scripts/BGController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGController : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 5.0f;//スクロールの速さ
+    [SerializeField]
+    private float startLine;//BG開始位置
+    [SerializeField]
+    private float deadLine;//BG終了位置
+
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(speed, 0, 0);//x座標をspeed分動かす
+
+        //もしdeadLineより大きくなったら
+        if (transform.position.x < deadLine)
+        {
+            transform.position = new Vector3(startLine, 0, 0);//startLineに戻る
+        }
+    }
+}
